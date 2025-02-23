ad4a5ceb79deaf9bde5761bd5ae7a89cc92c7f64
Anamika Tiwari <154797553+anamika-tiwari03@users.noreply.github.com>
2025-02-23 22:43:43 +0530

    Update app.js

--- Episode12/app.js
+++ Episode12/app.js
@@ -1,57 +1,3 @@
-//Synchronous: each instruction waits for the other instruction and executes in a sequence
-// console.log("one");
-// console.log("two");
-// console.log("three");
-
-//Asynchronous : Some lines will take more time for the execution that timw delay 
-// to overcome this asynchronous code execution will be used
-
-// console.log("one");
-// console.log("two");
-// setTimeout(() => {
-//     console.log("hello");
-// }, 2000);//timeout 2s= 2000ms
-// console.log("three");
-// console.log("four");
-
-//callback
-
-// function sum(a, b) {
-//     console.log(a + b);
-// }
-
-// function calculator(a, b, sumCallBack) {
-//     sumCallBack(a, b);
-// }
-
-// calculator(1, 2, sum);
-
-//callBack for setTimeout
-
-// const hello = () => {
-//     console.log("hello");
-// }
-// setTimeout(hello, 3000);
-
-//Callback Hell
-/*
-Callback hell: nested callbacks stacked below one another forming a pyramid structure(pyramid doom)
-This style of programming becomes difficult to understand and manage.
-*/
-// let age = 19;
-// if (age >= 18) {
-//     if (age >= 68) {
-//         console.log("Senior");
-//     } else {
-//         console.log("middle");
-//     }
-// } else {
-//     console.log("child");
-// }
-
-
-//callback hell example
-
 function getData(dataId, getNextData) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
@@ -64,15 +10,6 @@ function getData(dataId, getNextData) {
     });
 }
 
-// let p1 = getData(1);
-// p1.then((res) => {
-//     console.log(res);
-//     getData(2).then(() => {
-//         console.log(res);
-//     })
-// })
-
-//other method - promise chaining
 
 getData(1).then((res) => {
     return getData(2);
@@ -96,10 +33,7 @@ getData(1).then((res) => {
 //     });
 // });
 
-//Promises- eventual completion of task. it is an object in Js
-//It is a solution to callback hell
 //let promise = new Promise(resolve,reject)
-
 // let promise = new Promise((resolve, reject) => {
 //     console.log("I am a Promise");
 //     // resolve(123);
